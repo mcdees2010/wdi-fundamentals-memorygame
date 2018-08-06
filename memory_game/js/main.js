7b7379bafc5e029b1a70bdf06b753188d1c0a8ca
Maria <admin@admins-MacBook.local>
2018-08-06 13:46:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,43 @@
-var cardOne = "queen";
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}]
 
-var cardTwo = "king";
+var cardsInPlay = [];
 
-var cardThree = "queen";
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+}
+}
 
-var cardFour = "king";
+var flipCard = function(cardId){
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	checkForMatch();	
+}	
 
-console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardFour);
+flipCard(0);
+flipCard(2);
